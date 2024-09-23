ec60b98735144508dd23fa4e221a75fc4a107e09
Jesse Abdul <jesse.abdul@noaa.gov>
2024-09-23 10:39:29 -1000

    Updated project to implement the simplified preparatino and deployment scripts developed for the SQLPlus Query Metrics project

--- docker/src/py_scripts/lib/custom_functions.py
+++ docker/src/py_scripts/lib/custom_functions.py
@@ -2,6 +2,7 @@
 import time
 import json
 import pprint
+import pytz
 
 # include selenium libraries
 from datetime import datetime 
@@ -160,8 +161,6 @@ def wait_for_response (stale_element, by_method, clickable_element_id, start_tim
         EC.element_to_be_clickable((by_method, clickable_element_id)))      
     finally:
         
-
-        
         log_value ("The try section has finished executing, execute the finally section", print_log_messages)
         
         # stop the timer
@@ -187,7 +186,22 @@ def wait_for_response (stale_element, by_method, clickable_element_id, start_tim
         # save the screenshot from the web request/page load
         driver.save_screenshot('/app/data/screenshots/'+screenshot_file)
 
-        fp.write('"'+app_config.app_name+'","'+project_scenario_config.container_location+'","'+project_scenario_config.app_location+'","'+time.strftime('%m/%d/%Y %I:%M:%S %p', time.localtime(start_timer/1000))+'","'+driver.title+'","'+web_action+'","'+str(total_files)+'","'+str(total_file_size)+'","'+str(round(total_time_ms / 1000, 3))+'","'+screenshot_file+'"'+"\n")
+
+
+        # define the UTC and HST timezones:
+        utc_timezone = pytz.timezone("UTC")
+        hst_timezone = pytz.timezone("Pacific/Honolulu")
+
+        # convert the start_timer to a datetime object so it can be used to calculate the HST date/time based on the UTC date/time
+        start_datetime_utc = utc_timezone.localize(start_timer /1000)
+        start_datetime_hst = start_datetime_utc.astimezone(hst_timezone)
+
+
+        log_value("start_datetime_utc is: "+start_datetime_utc.strftime('%m/%d/%Y %I:%M:%S %p'), print_log_messages)
+        log_value("start_datetime_hst is: "+start_datetime_hst.strftime('%m/%d/%Y %I:%M:%S %p'), print_log_messages)
+
+
+        fp.write('"'+app_config.app_name+'","'+project_scenario_config.container_location+'","'+project_scenario_config.app_location+'","'+start_datetime_utc.strftime('%m/%d/%Y %I:%M:%S %p')+'","'+start_datetime_hst.strftime('%m/%d/%Y %I:%M:%S %p')+'","'+driver.title+'","'+web_action+'","'+str(total_files)+'","'+str(total_file_size)+'","'+str(round(total_time_ms / 1000, 3))+'","'+screenshot_file+'"'+"\n")
 
         return True
 
--- docker/src/sh_scripts/config/project_deploy_config.sh
+++ docker/src/sh_scripts/config/project_deploy_config.sh
@@ -4,4 +4,4 @@
 project_path="ibbs-web-app-metrics"
 
 # project git URL value
-git_url="git@github.com:noaa-pifsc/PIFSC-Tools-Web-App-Metrics-IBBS.git"
+git_url="--branch branch_OCI_deployment https://github.com/noaa-pifsc/PIFSC-Tools-Web-App-Metrics-IBBS.git"
